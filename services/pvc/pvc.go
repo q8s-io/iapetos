56b53d33eece853f3e34e71c296e81209d364903
千夜 <70data@gmail.com>
2022-05-22 16:20:58 +0800

    Update pvc.go

--- services/pvc/pvc.go
+++ services/pvc/pvc.go
@@ -11,8 +11,8 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	iapetosapiv1 "w.src.corp.qihoo.net/data-platform/infra/iapetos.git/api/v1"
-	"w.src.corp.qihoo.net/data-platform/infra/iapetos.git/services"
+	iapetosapiv1 "github.com/q8s-io/iapetos/api/v1"
+	"github.com/q8s-io/iapetos/services"
 )
 
 type PVCService struct {
